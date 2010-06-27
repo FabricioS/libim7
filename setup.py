512bf72e386f084a2fa191623665b4eb8b9c94ba
Fabrice Silva <silva@lma.cnrs-mrs.fr>
2010-06-27 02:08:20 -0300

    Autoconfiguration of compilers

--- setup.py
+++ setup.py
@@ -5,12 +5,21 @@
 
 """
 """
-import os
+import sys,os
 from distutils.core import setup, Extension
+from distutils.command.build_ext import build_ext
 
 name = 'im7'
 version = 0.1
 
+# Configure C and fortran compilers
+if sys.platform in ('win32', 'cygwin'):
+    build_ext.compiler = "mingw32"
+    build_ext.fcompiler = "gnu"
+else:
+    build_ext.compiler = "unix"
+    build_ext.fcompiler = "gnu95"
+    
 setup(name=name, version=version, \
     ext_modules=[Extension('_'+name, 
         sources=['src/ReadIM7.cpp', 'src/ReadIMX.cpp'],
